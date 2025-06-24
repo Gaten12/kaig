05cbc1630baab27986c569da5cf9355eef08bef8
Gaten12 <galdimergaming@gmail.com>
2025-06-24 23:06:46 +0700

    mbledos

--- lib/screens/customer/utama/beranda_content.dart
+++ lib/screens/customer/utama/beranda_content.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kaig/screens/customer/utama/tiket/jadwal_krl_viewer_screen.dart';
 
 class BerandaContent extends StatelessWidget {
   final Function(int) onNavigateToTab;
@@ -352,17 +353,9 @@ class BerandaContent extends StatelessWidget {
                   isPrimary: false,
                   isSmallScreen: isSmallScreen, // Pass isSmallScreen
                   onTap: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: const Text(
-                            'Fitur Commuter Line sedang dalam pengembangan'),
-                        backgroundColor: Colors.grey[700],
-                        behavior: SnackBarBehavior.floating,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        margin: const EdgeInsets.all(16),
-                      ),
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const JadwalKrlViewerScreen())
                     );
                   },
                 ),
